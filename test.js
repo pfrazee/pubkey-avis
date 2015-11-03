858cc656c617c25559165abb3e07e4c4862a9b66
David Guttman <david@davidguttman.com>
2015-11-03 07:57:23 -0800

    test: pull pubkey from location hash
    also allows pasting patchwork id

--- test.js
+++ test.js
@@ -1,17 +1,37 @@
 var pubkeyAvis = require('./index')
 
-// generate public key
-var pubkeyBuf = new ArrayBuffer(32)
-var pubkeyUints = new Uint8Array(pubkeyBuf)
-for (var i=0; i < 32; i++)
-  pubkeyUints[i] = (Math.random() * 256)|0
-console.log(pubkeyUints)
+if (window.location.hash) {
+  var pubkeyBase64 = window.location.hash.replace(/^#/, '')
+  var str = atob(pubkeyBase64)
 
-// render public key
-var pubkeyBase64 = btoa(String.fromCharCode.apply(null, pubkeyUints))
+  var pubkeyBuf = new ArrayBuffer(32)
+  var pubkeyUints = new Uint8Array(pubkeyBuf)
+  for (var i=0; i < 32; i++)
+    pubkeyUints[i] = str.charCodeAt(i)
+} else {
+  // generate public key
+  var pubkeyBuf = new ArrayBuffer(32)
+  var pubkeyUints = new Uint8Array(pubkeyBuf)
+  for (var i=0; i < 32; i++)
+    pubkeyUints[i] = (Math.random() * 256)|0
+  console.log(pubkeyUints)
+
+  // render public key
+  var pubkeyBase64 = btoa(String.fromCharCode.apply(null, pubkeyUints))
+}
+
 
 document.title = 'pubkey avis'
-append(document.body, createElement('h1', pubkeyBase64))
+// append(document.body, createElement('h1', pubkeyBase64))
+var tfield = createElement('input')
+attr(tfield, {
+  value: pubkeyBase64,
+  style: {width: '90%', fontSize: '200%'}})
+tfield.addEventListener('change', function () {
+  var pubKeyStr = tfield.value.replace(/^@/, '')
+  window.location.hash = pubKeyStr.split('.')[0]
+})
+append(document.body, tfield)
 
 Object.keys(pubkeyAvis).forEach(function (algo) {
   var canvas = attr(createElement('canvas'), {
@@ -42,7 +62,15 @@ function append (parent, child) {
 
 function attr (el, props) {
   for (key in props) {
-    el[key] = props[key]
+    if (key === 'style') {
+      for (sk in props.style) {
+        el.style[sk] = props.style[sk]
+      }
+    } else {
+      el[key] = props[key]
+    }
   }
   return el
 }
+
+window.addEventListener('hashchange', window.location.reload.bind(window.location))
